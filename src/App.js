de092029681191b25ed2458333510e3f9873f29b
pgeovany <pgeovany@dcc.ufrj.br>
2022-05-05 23:13:43 -0300

    basic react rendering done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Corpo from "./Corpo";
+import FundoMobile from "./FundoMobile";
+import Navbar from "./Navbar";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Corpo />
+            <FundoMobile />
+        </div>
+    );
+}
